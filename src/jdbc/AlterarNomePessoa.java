6c2eed54ec6da20d074051aa008da83cec69bc7b
marciliodevjava <marciliodevjava@gmail.com>
2022-05-20 17:53:37 -0300

    JAVA JDBC

---
+++ src/jdbc/AlterarNomePessoa.java
@@ -0,0 +1,50 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class AlterarNomePessoa {
+
+	public static void main(String[] args) throws SQLException{
+		
+		Scanner entrada = new Scanner(System.in);
+		Connection conexao = FabricaConexao.getConexao();
+		
+		
+		System.out.println("Informe o codigo da pessoa: ");
+		int codigo = entrada.nextInt();
+		
+		String select = "SELECT codigo, nome FROM pessoas WHERE codigo = ?";
+		String update = "UPDATE pessoas SET nome = ? WHERE codigo = ?";
+		
+		PreparedStatement stmt = conexao.prepareStatement(select);
+		stmt.setInt(1, codigo);
+		ResultSet resultado = stmt.executeQuery();
+		
+		if(resultado.next()) {
+			Pessoa p = new Pessoa(resultado.getInt(1), resultado.getString(2));
+			
+			System.out.println("O nome atual é " + p.getNome());
+			entrada.nextLine();
+			
+			System.out.println("Informe o novo nome: ");
+			String novoNome = entrada.nextLine();
+			
+			stmt.close();
+			stmt = conexao.prepareStatement(update);
+			stmt.setString(1, novoNome);
+			stmt.setInt(2, codigo);
+			stmt.execute();
+			
+			System.out.println("Pessoa alterada com sucesso!");
+		} else {
+			System.out.println("Pessoa não encontrada!");
+		}
+		
+		entrada.close();
+		conexao.close();
+	}
+}
